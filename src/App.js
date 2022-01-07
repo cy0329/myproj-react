2021b37b2663d31563a33bde5178f829154cd3dd
Yeol Choi <chlduf153@gmail.com>
2022-01-07 13:25:49 +0900

    TopNav 완성, root로 접속시 /reviews로 이동

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import TopNav from 'components/TopNav';
 import Login from 'pages/accounts/Login';
 import Profile from 'pages/accounts/Profile';
 import ReviewList from 'pages/reviews/ReviewList';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <TopNav />
       <Routes>
+        <Route path="/" element={<Navigate to="/reviews/" />} />
         <Route path="/accounts/login/" element={<Login />} />
         <Route path="/accounts/profile/" element={<Profile />} />
         <Route path="/reviews/" element={<ReviewList />} />
