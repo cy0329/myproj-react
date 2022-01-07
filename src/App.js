3198ad38a378013a654584baeaab94d3763c9552
Yeol Choi <chlduf153@gmail.com>
2022-01-07 15:43:40 +0900

    Alert 컴포넌트 개발 예시 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import TopNav from 'components/TopNav';
 import Login from 'pages/accounts/Login';
 import Profile from 'pages/accounts/Profile';
+import Components from 'pages/examples/Components';
 import ReviewList from 'pages/reviews/ReviewList';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
         <Route path="/accounts/login/" element={<Login />} />
         <Route path="/accounts/profile/" element={<Profile />} />
         <Route path="/reviews/" element={<ReviewList />} />
+        <Route path="/examples/components/" element={<Components />} />
       </Routes>
     </div>
   );
