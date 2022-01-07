311ddd44a83d3afb392adcb6449319589e75c4c4
Yeol Choi <chlduf153@gmail.com>
2022-01-07 14:33:19 +0900

    DebugState 컴포넌트를 활용한 상탯값 확인, loading/error상탯값 처리 추가

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -1,32 +1,57 @@
 import Axios from 'axios';
-import { useEffect } from 'react';
+import DebugStates from 'components/DebugStates';
+import { useEffect, useState } from 'react';
 
-function ReviewList() {
+function PageReviewList() {
+  const [reviewList, setReviewList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     refetch();
   }, []);
 
   const refetch = () => {
+    setError(null);
+    setLoading(true);
     const url = 'http://127.0.0.1:8000/shop/api/reviews/';
     // Promise 객체 --> then, catch 지원, 체이닝 가능
     Axios.get(url)
-      .then((response) => {
+      .then(({ data }) => {
         console.group('정상 응답');
-        console.log(response);
+        console.log(data);
         console.groupEnd();
+        setReviewList(data);
       })
       .catch((error) => {
         console.group('에러 응답');
         console.log(error);
         console.groupEnd();
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
   return (
     <div>
       <h2>Review List</h2>
+
+      {loading && <div>Loading...</div>}
+      {error && <div>통신 중에 오류가 발생했습니다.</div>}
+
+      <button
+        onClick={() => refetch()}
+        className="bg-yellow-400 hover:bg-red-300"
+      >
+        새로고침
+      </button>
+
+      <hr />
+      <DebugStates loading={loading} error={error} reviewList={reviewList} />
     </div>
   );
 }
 
-export default ReviewList;
+export default PageReviewList;
