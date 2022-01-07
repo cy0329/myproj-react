4a9a14adc58ca737061f514e28bbf7e0645585c9
Yeol Choi <chlduf153@gmail.com>
2022-01-07 16:36:27 +0900

    Review Delete API를 활용하여 리뷰 삭제를 구현

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,9 +1,28 @@
 import Rating from './Rating';
 
-function Review({ review }) {
+// review : 보여질 리뷰 객체
+// handleDelete : 인자 없는 함수, 삭제 버튼 클릭 시에 호출됨
+function Review({ review, handleEdit, handleDelete }) {
   const { content, score } = review;
+
+  // TODO : handleEdit, handleDelete 호출에 대한 방어적 코드 작성
+
   return (
     <div className="bg-yellow-100 border border-yellow-400 my-1 p-1">
+      <div>
+        <span
+          onClick={() => handleEdit()}
+          className="hover:text-blue-400 cursor-pointer mr-1"
+        >
+          수정
+        </span>
+        <span
+          onClick={() => handleDelete()}
+          className="hover:text-red-400 cursor-pointer mr-1"
+        >
+          삭제
+        </span>
+      </div>
       {content}
       <Rating score={score} />
     </div>
--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -35,6 +35,32 @@ function PageReviewList() {
       });
   };
 
+  const editReview = () => {};
+
+  const deleteReview = (deletingReview) => {
+    const { id: deletingReviewId } = deletingReview;
+    const url = `http://127.0.0.1:8000/shop/api/reviews/${deletingReviewId}/`;
+
+    setLoading(true);
+    setError(null);
+
+    Axios.delete(url)
+      .then(() => {
+        console.log('삭제 성공');
+        // 선택1 : 삭제된 항목만 상탯값에서 제거
+        setReviewList((prevReviewList) =>
+          prevReviewList.filter((review) => review.id !== deletingReviewId),
+        );
+        // 선택2 : 전체를 새로고침
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
       <h2>Review List</h2>
@@ -50,7 +76,12 @@ function PageReviewList() {
       </button>
 
       {reviewList.map((review) => (
-        <Review key={review.id} review={review} />
+        <Review
+          key={review.id}
+          review={review}
+          handleEdit={editReview}
+          handleDelete={() => deleteReview(review)}
+        />
       ))}
 
       <hr />
