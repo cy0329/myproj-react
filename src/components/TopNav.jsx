3aff8cb90e20fc75845e97ac643155c0cd9a157d
Yeol Choi <chlduf153@gmail.com>
2022-01-11 17:11:57 +0900

    TopNav에서 블로그만 남기기

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,7 +4,7 @@ function TopNav() {
   return (
     <div className="my-3">
       <ul className="flex gap-4">
-        <li>
+        {/* <li>
           <MyLink to="/accounts/login/">로그인</MyLink>
         </li>
         <li>
@@ -12,11 +12,11 @@ function TopNav() {
         </li>
         <li>
           <MyLink to="/reviews/">리뷰</MyLink>
-        </li>
+        </li> */}
         <li>
           <MyLink to="/blogs/">블로그</MyLink>
         </li>
-        <li>
+        {/* <li>
           <MyLink to="/examples/components/">컴포넌트 예시</MyLink>
         </li>
         <li>
@@ -33,7 +33,7 @@ function TopNav() {
         </li>
         <li>
           <MyLink to="/examples/context-api-2/">ContextApiSample2</MyLink>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
