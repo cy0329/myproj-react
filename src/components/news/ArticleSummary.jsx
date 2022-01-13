b3ce987dfb0bfc3498346180507e493f23607a62
Yeol Choi <chlduf153@gmail.com>
2022-01-13 10:37:50 +0900

    article list/detail 컴포넌트에 photo 노출

--- src/components/news/ArticleSummary.jsx
+++ src/components/news/ArticleSummary.jsx
@@ -2,8 +2,15 @@ import { Link } from 'react-router-dom';
 
 function ArticleSummary({ article }) {
   return (
-    <div>
-      요약 :<Link to={`/news/${article.id}/`}>{article.title}</Link>
+    <div className="mb-2">
+      {article.photo && (
+        <img
+          src={article.photo}
+          alt={article.title}
+          className="w-7 h-7 mr-1 rounded inline"
+        />
+      )}
+      <Link to={`/news/${article.id}/`}>{article.title}</Link>
     </div>
   );
 }
