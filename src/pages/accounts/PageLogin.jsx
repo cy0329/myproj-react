6f3bb7a0de7541c31944906ed7f484abc3c24eeb
Yeol Choi <chlduf153@gmail.com>
2022-01-17 16:53:48 +0900

    PageLogin. username/password를 통한 token 획득 구현

--- src/pages/accounts/PageLogin.jsx
+++ src/pages/accounts/PageLogin.jsx
@@ -1,7 +1,80 @@
+// input[type=text]     name=username
+// input[type=password] name=password
+// userFieldValues 훅 쓰고
+// PageLogin 컴포넌트 내에서 fieldVqlues 상탯값 노출
+
+import { useApiAxios } from 'api/base';
+import DebugStates from 'components/DebugStates';
+import useFieldValues from 'hooks/useFieldValues';
+import { useNavigate } from 'react-router-dom';
+
+const INIT_FIELD_VALUES = {
+  username: '',
+  password: '',
+};
+
 function PageLogin() {
+  const { fieldValues, handleFieldChange } = useFieldValues(INIT_FIELD_VALUES);
+  const navigate = useNavigate();
+  const [{ loading, error }, loginRequest] = useApiAxios(
+    {
+      url: `/accounts/api/token/`,
+      method: 'POST',
+    },
+    { manual: true },
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginRequest({ data: fieldValues }).then((response) => {
+      const { access, refresh } = response.data;
+      console.log('access :', access);
+      console.log('refresh :', refresh);
+
+      navigate('/');
+    });
+  };
+
   return (
     <div>
       <h2>Login</h2>
+
+      {error?.response?.status === 401 && (
+        <div className="text-red-400">로그인에 실패했습니다.</div>
+      )}
+
+      <h2>username</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={fieldValues.username}
+          onChange={handleFieldChange}
+          placeholder="ID를 입력해주세요"
+          className="p-2 border-2 focus:outline-none focus:border-blue-400 rounded-lg mb-2"
+        />
+
+        <h2>password</h2>
+        <input
+          type="password"
+          name="password"
+          value={fieldValues.password}
+          onChange={handleFieldChange}
+          placeholder="PassWord"
+          className="p-2 border-2 focus:outline-none focus:border-blue-400 rounded-lg  mb-2"
+        />
+        <div>
+          <button className="rounded-lg bg-blue-700 hover:bg-purple-300 p-2 text-lg text-white hover:text-black">
+            로그인
+          </button>
+        </div>
+      </form>
+      <DebugStates
+        username={fieldValues.username}
+        password={fieldValues.password}
+      />
+      {/* <div>ID : {fieldValues.username}</div>
+      <div>PW : {fieldValues.password}</div> */}
     </div>
   );
 }
